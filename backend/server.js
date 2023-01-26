157c315489e159318b0674b858aaf8d28b19f68e
Jidan Maharjan <jidanmaharjan1@gmail.com>
2023-01-26 19:02:28 +0545

    static path changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const mongoose = require("mongoose");
 
@@ -30,6 +31,11 @@ const event = require("./routes/event");
 app.use("/v1/auth", auth);
 app.use("/v1/category", category);
 app.use("/v1/event", event);
+
+app.use(express.static(path.join(__dirname, "../frontend/build/")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(
